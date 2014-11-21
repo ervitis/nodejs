2a27acbfb98fa420fbbcfe992ef1d3f54f1866ea
victormartin <victormartin@github.com>
2014-11-21 16:49:25 +0100

    First commit

---
+++ helloWorld.js
@@ -0,0 +1,9 @@
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(8080);
+
+console.log('CursoNodejs escuchando en el puerto 8080');
